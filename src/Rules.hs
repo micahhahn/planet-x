e20cfd04d4fbe022340ab1d9ef6b305258e5501e
Micah Hahn <micah.s.hahn@gmail.com>
2021-05-30 15:17:09 -0400

    Add combo function

--- src/Rules.hs
+++ src/Rules.hs
@@ -10,10 +10,16 @@ import PlanetX
 newtype Solution = Solution { unSolution :: Word64 }
     deriving (Show)
 
-c = V.fromList [ Astroid, Astroid, Astroid, Astroid, Comet, Comet, DwarfPlanet, DwarfPlanet, DwarfPlanet, DwarfPlanet, EmptySpace, EmptySpace, EmptySpace, EmptySpace, EmptySpace, GasCloud, GasCloud, PlanetX ]
+objects = fmap (uncurry replicate) [(2, Comet), (4, Astroid), (4, DwarfPlanet), (2, GasCloud), (6, EmptySpace)]
 
 packSolution :: Vector Object -> Solution
 packSolution = Solution . foldr (\o w -> shiftL w 3 .|. fromIntegral (fromEnum o)) 0
 
 unpackSolution :: Solution -> Vector Object
 unpackSolution (Solution w) = V.fromList $ fmap (\i -> toEnum . fromIntegral $ shiftR w (3 * i) .&. 7) [0..17]
+
+combo :: [[a]] -> [[a]]
+combo (a:as) = f [] a as
+    where f :: [[a]] -> [a] -> [[a]] -> [[a]]
+          f ls [] (r:rs) = f ls r rs
+          f ls (c:cs) (r:rs) = fmap (c:) (f (cs:ls) r rs) ++ f ((c:cs):ls) r rs
